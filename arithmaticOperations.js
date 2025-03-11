6a0ecc15509621271fdf3b0f8030d10319fe857e
Shahla5583 <shahlafathuma@gmail.com>
2025-03-11 23:06:01 +0530

    Add files via upload

---
+++ arithmaticOperations.js
@@ -0,0 +1,15 @@
+//Perform arithmetic operations
+
+let x=5;
+let y=8;
+
+var a =1;
+var b=2;
+var b=3;
+
+console.log(x+y);
+console.log(x-y);
+console.log(x*y);
+console.log(y/x);
+
+console.log(a+b);
